c8f078dc66b13120bf1e6e42fcdb51d7f53aa5b5
dojolew <lewis.yearsley@dojo.tech>
2023-06-29 16:19:13 +0100

    fix: remove flag from example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -10,7 +10,6 @@ export default function App() {
   const pay = () => {
     startPaymentFlow({
       intentId: token,
-      isProduction: false,
     }).then((res) => {
       setResult(res);
     });
